51880e637746c6d459160cc4bd7b09b3e5fc9189
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-09-16 16:36:38 +0900

    docs/ check complited functions

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,6 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import racingcar.console.InputConsole;
 import racingcar.console.ResultConsole;
 import racingcar.race.Race;
@@ -13,7 +10,7 @@ public class Application {
   public static void main(String[] args) {
     // TODO: 프로그램 구현
 
-    // Console 부분
+    // InputConsole 부분
     InputConsole input = new InputConsole();
     String[] cars = input.inputCarNames();
     int tryNumberInt = input.inputTryNumber();
@@ -23,7 +20,7 @@ public class Application {
     HashMap<String,Integer> carInfo = race.setCarInfo(cars);
     race.startRace(tryNumberInt,cars,carInfo);
 
-    // 우승자 출력
+    // ResultConsole 부분
     ResultConsole resultConsole = new ResultConsole();
     int maxDistance = resultConsole.getMaxDistance(carInfo);
     resultConsole.printResult(carInfo,maxDistance);
