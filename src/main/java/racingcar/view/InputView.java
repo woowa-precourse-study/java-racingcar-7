4ccc7e721efd06dd27562cb7960674cc726558ac
kyeonghyeon <khcho1492@gmail.com>
2025-09-24 20:01:03 +0900

    Feat(Input): 사용자 입력 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NUMBER_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    public String readCarName() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        return Console.readLine().strip();
+    }
+
+    public String readNumber() {
+        System.out.println(NUMBER_INPUT_MESSAGE);
+        return Console.readLine().strip();
+    }
+}
