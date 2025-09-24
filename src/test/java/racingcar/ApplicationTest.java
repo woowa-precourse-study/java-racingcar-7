02a2b3a27f649017e266867e0e37c179f8eb6c0d
kyeonghyeon <khcho1492@gmail.com>
2025-09-24 19:38:49 +0900

    Feat(Application): 모든 로직 Apllication.java에서 구현

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,10 +24,95 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 공동우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,경현,woni", "3");
+                    // 두 명의 진행 상태 확인
+                    // "최종 우승자 : pobi, woni" 혹은 "최종 우승자 : woni, pobi" 둘 다 허용
+                    assertThat(output()).contains("pobi : ---", "경현 : ---", "woni : ---",
+                            "최종 우승자 : pobi, 경현, woni");
+                },
+                MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 쉼표_2개_이상_연속_입력_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,,woni", "2");
+                    // 두 명의 진행 상태 확인
+                    // "최종 우승자 : pobi"
+                    assertThat(output()).contains("pobi : --", "woni : -", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 입력값_양쪽_공백_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("   pobi,woni   ", "1");
+                    // 두 명의 진행 상태 확인
+                    // "최종 우승자 : pobi"
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 자동차이름_숫자_기호_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi1,$woni", "1");
+                    // 두 명의 진행 상태 확인
+                    // "최종 우승자 : pobi"
+                    assertThat(output()).contains("pobi1 : -", "$woni : ", "최종 우승자 : pobi1");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 입력값_공백_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("   ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 입력값_null_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("   ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차이름_5자_초과_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차이름_공백포함_5자_초과_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,kh cho", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차개수_0개_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" , , , ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
