147cce7a423637cdc6df7d421276c014f71037fc
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-09-27 04:55:41 +0900

    feat (controller) : 게임 결과 출력 기능 추가

--- src/main/java/racingcar/controller/PrintMessage.java
+++ src/main/java/racingcar/controller/PrintMessage.java
@@ -5,5 +5,6 @@ public class PrintMessage {
 
     public static final String REQUEST_CAR_NAME="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String REQUEST_TRY_COUNT="시도할 회수는 몇회인가요?";
-
+    public static final String RACING_RESULT_TITLE="실행 결과";
+    public static final String RACING_RESULT_WINNER="최종 우승자 : ";
 }
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,11 +1,14 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
 import racingcar.exception.ErrorMessage;
 import racingcar.service.RacingService;
 
-public class RacingController {
+import java.util.ArrayList;
+import java.util.List;
 
+public class RacingController {
 
     private final RacingService racingService;
 
@@ -15,20 +18,41 @@ public class RacingController {
 
     // 컨트롤러 메인 함수
     public void playGame(){
+        // 자동차 이름 입력 요청
         printMessage(PrintMessage.REQUEST_CAR_NAME);
         String carString=getInput();
         checkBlank(carString);
+        List<String> carNameList=splitCarList(carString);
 
+        // 시도 횟수 입력 요청
         printMessage(PrintMessage.REQUEST_TRY_COUNT);
         String tryString=getInput();
         checkBlank(tryString);
+
+        // 결과 출력
+        printMessage(PrintMessage.RACING_RESULT_TITLE);
+
+        // 자동차 객체 생성
+        int tryCount=Integer.parseInt(tryString);
+        List<Car> cars=carNameList.stream()
+                                .map(Car::new)
+                                .toList();
+
+        // 레이스
+        for(int i=0;i<tryCount;i++){
+            racingService.doRace(cars);
+        }
+
+        // 우승자 출력
+        List<String> winners=racingService.getWinners(cars);
+        printMessage(PrintMessage.RACING_RESULT_WINNER + String.join(", ",  winners));
 
 
     }
 
     // 입력 요청 메세지
     public void printMessage(String message){
-        System.out.print(message);
+        System.out.println(message);
     }
 
     // 입력 받음
@@ -38,12 +62,16 @@ public class RacingController {
     }
 
     // 입력값 유효성 검사
-    public Boolean checkBlank(String inputString){
+    public void checkBlank(String inputString){
         if (inputString.isBlank()){
             throw new IllegalArgumentException(ErrorMessage.EMPTY_STRING);
         }
-        return true;
     }
 
+    // 자동차 입력값 가공
+    public List<String> splitCarList(String inputString){
+        List<String> carList=List.of(inputString.split(","));
+        return carList;
+    }
 
 }
