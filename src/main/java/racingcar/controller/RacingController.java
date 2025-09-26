92bacf2fad59dc73c4c6adb0288840b21eaceb30
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-09-27 05:34:53 +0900

    fix (controller) : 입력값 유효성 검사 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,12 +1,15 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import net.bytebuddy.build.Plugin;
 import racingcar.domain.Car;
 import racingcar.exception.ErrorMessage;
 import racingcar.service.RacingService;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RacingController {
 
@@ -23,11 +26,14 @@ public class RacingController {
         String carString=getInput();
         checkBlank(carString);
         List<String> carNameList=splitCarList(carString);
+        checkCarNameLength(carNameList);
+        checkCarNameUnique(carNameList);
 
         // 시도 횟수 입력 요청
         printMessage(PrintMessage.REQUEST_TRY_COUNT);
         String tryString=getInput();
         checkBlank(tryString);
+        checkTryCount(tryString);
 
         // 결과 출력
         printMessage(PrintMessage.RACING_RESULT_TITLE);
@@ -72,6 +78,34 @@ public class RacingController {
     public List<String> splitCarList(String inputString){
         List<String> carList=List.of(inputString.split(","));
         return carList;
+    }
+
+    // 입력값 유효성 검사
+    public void checkCarNameLength(List<String> carList){
+        for (String carName:carList){
+            if (carName.length()>=6){
+                throw new IllegalArgumentException(ErrorMessage.TOO_LONG_CAR_NAME);
+            }
+        }
+    }
+
+    // 입력값 유효성 검사
+    public void checkCarNameUnique(List<String> carList){
+        Set<String> seen = new HashSet<>();
+        for (String carName : carList) {
+            if (!seen.add(carName)) {  // add()가 false면 이미 존재
+                throw new IllegalArgumentException(ErrorMessage.SAME_CAR_NAME);
+            }
+        }
+    }
+
+    public void checkTryCount(String inputString){
+        try{
+            Integer.parseInt(inputString);
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TRY);
+        }
+
     }
 
 }
--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -8,6 +8,5 @@ public class ErrorMessage {
     public static final String TOO_LONG_CAR_NAME="5글자 이하의 자동차 이름을 입력해주세요";
     public static final String SAME_CAR_NAME="동일한 자동차 이름은 허용하지 않습니다.";
     public static final String INVALID_TRY="올바르지 않은 입력 형식입니다. 숫자를 입력해주세요";
-    public static final String TOO_MANY_TRY_COUNT="0이상 9 이하의 값을 입력해주세요";
 
 }
