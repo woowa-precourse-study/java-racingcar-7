725734b6033cad4f6f901bd780259eb6f758dd74
sem_in_github <semin6634@naver.com>
2025-09-27 22:20:41 +0900

    feat($domain-carnameverifier): CarNameVerifier 추가

---
+++ src/main/java/racingcar/domain/CarNameVerifier.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO: split 기능도 추가됨 -> 클래스명 바꾸거나 분리하기
+public class CarNameVerifier {
+
+    public List<Car> createCarList(String rawStringOfCarNames) {
+
+        List<Car> carList = new ArrayList<>();
+
+        if(rawStringOfCarNames == null || rawStringOfCarNames.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 공백은 안 받아요");
+        }
+
+        // CarNameVerifier
+        // "jenny  "거나 "   "
+        String[] carNamesArray = rawStringOfCarNames.split(",");
+        for(String carName : carNamesArray) {
+            if(carName.isBlank())  throw new IllegalArgumentException("[ERROR] 공백은 안 받아요");
+            if(carName.length() > 5) throw new IllegalArgumentException("[ERROR] 이름이 5자를 초과해");
+            carList.add(new Car(carName.strip()));
+        }
+
+        return carList;
+    }
+
+}
