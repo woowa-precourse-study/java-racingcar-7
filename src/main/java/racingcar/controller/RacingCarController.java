8b24bc7e4c42bf3168f72fd5e1f67feaba965368
sem_in_github <semin6634@naver.com>
2025-09-27 22:17:46 +0900

    feat($RacingCarController): 컨트롤러 추가

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.service.RacingCarService;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+public class RacingCarController {
+    private final InputView inputView = new InputView();
+    private final RacingCarService racingCarService = new RacingCarService();
+    private final ResultView resultView = new ResultView();
+
+    public void run(){
+        resultView.printResult(racingCarService.playRacingCar(inputView.readCarNames(), inputView.readRound()));
+    }
+}
