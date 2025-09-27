9ce4784a87dda729dd7e05384b94cd695897a4d4
sem_in_github <semin6634@naver.com>
2025-09-27 22:19:54 +0900

    feat($domain-car): Car 도메인 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+public class Car {
+    private String carName;
+    private int position;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.position = 0;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position++;
+    }
+}
