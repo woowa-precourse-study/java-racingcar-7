92e6cec826e8cb16ca13692a8058b72e594e3ef6
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-01-01 23:01:34 +0900

    feat: 시도횟수 입력 및 입력값 검사 기능 구현

--- src/main/java/racingcar/exception/Validator.java
+++ src/main/java/racingcar/exception/Validator.java
@@ -31,5 +31,20 @@ public interface Validator {
         }
     }
 
+    static int validateIsNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+        }
+    }
+
+    static void validatePositiveNumber(String input) {
+        int value = validateIsNumber(input);
+        if (value < 0) {
+            throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
+        }
+    }
+
 }
 
