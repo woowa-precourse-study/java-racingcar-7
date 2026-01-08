073271d44889c1a322ee48a365c6e803535086ed
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-01-08 23:22:14 +0900

    3차 (초기화)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,7 @@
 package racingcar;
 
-import racingcar.controller.RacingController;
-import racingcar.service.RacingService;
-
 public class Application {
     public static void main(String[] args) {
-        RacingService service = new RacingService();
-        RacingController controller = new RacingController(service);
-        try {
-            controller.run();
-        } catch (IllegalStateException e) {
-            System.out.println(e.getMessage());
-        }
+        // TODO: 프로그램 구현
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,10 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -28,52 +24,12 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 기능_반복_테스트() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni,jun", "5");
-                    assertThat(output()).contains("pobi : -----", "woni : ----","jun : -----", "최종 우승자 : pobi, jun");
-                },
-                MOVING_FORWARD, STOP, MOVING_FORWARD
-        );
-    }
-
-    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
-
-    @Test
-    void 경주자동차입력_이름중복_예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi, jun, pobi", "2"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"pobi, jun,, pobi",",,pobi, jun, pobi","pobi, jun, pobi,,"})
-    void 경주자동차입력_쉼표기준분리_빈값_예외_테스트(String text) {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException(text, "2"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("[ERROR] 빈 값이 포함되어 있습니다.")
-        );
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"a","-2",""})
-    void 시도횟수입력_양수숫자아님_예외_테스트(String text) {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", text))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
 
     @Override
     public void runMain() {
